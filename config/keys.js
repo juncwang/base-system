349ff92001db652d475dfaa7bc92873913c25d04
juncwang <juncwang@live.com>
2019-05-08 13:11:25 +0800

    updata

--- config/keys.js
+++ config/keys.js
@@ -2,15 +2,3 @@ module.exports = {
     MongoURI: "mongodb://base:system@127.0.0.1:27017/base-system?authSource=admin",
     jwtSecret:'secret'
 }
-
-// jwt.verify(req.headers.token, jwtSecret, (err, data) => {
-//     if(err){
-//         let data = {
-//             msg: 'token 过期'
-//         }
-//         console.log(err)
-//         res.status(401).json(data)
-//     }else{
-        
-//     }
-// })
